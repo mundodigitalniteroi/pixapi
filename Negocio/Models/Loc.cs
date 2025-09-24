74feb15ff32478b5cd03892a609d784e59965c2e
blawerneck <blawerneck1@gmail.com>
2025-09-24 14:22:06 -0300

    Ajuste Int to long
    Ajuste de variável ID de int para LONG

--- Negocio/Models/Loc.cs
+++ Negocio/Models/Loc.cs
@@ -8,7 +8,7 @@ namespace Negocio.Models
     public class Loc
     {
         [JsonProperty("id")]
-        public int Id { get; set; }
+        public long Id { get; set; }
 
         [JsonProperty("location")]
         public string Location { get; set; }
